89c1f69e62a8222fc0941fe01681028c5d26f571
Dominique Boeckli <dboeckli@gmail.com>
2025-03-31 19:35:10 +0200

    Feature/implement author dao (#1)
    * implement find author
    
    * implement create author
    
    * implement update author
    
    * implement delete author

--- src/main/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/AuthorDaoImpl.java
+++ src/main/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/AuthorDaoImpl.java
@@ -1,38 +1,64 @@
 package ch.dboeckli.guru.jpa.hibernate.dao.dao;
 
 import ch.dboeckli.guru.jpa.hibernate.dao.domain.Author;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class AuthorDaoImpl implements AuthorDao {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManagerFactory emf;
 
     @Override
     public Author getById(Long id) {
-        return null;
+        return getEntityManager().find(Author.class, id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        TypedQuery<Author> query = getEntityManager().createQuery(
+            "SELECT author FROM Author author WHERE author.firstName = :first_name and author.lastName = :last_name", Author.class);
+        query.setParameter("first_name", firstName);
+        query.setParameter("last_name", lastName);
+
+        return query.getSingleResult();
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(author);
+        em.flush();
+        em.getTransaction().commit();
+        return author;
     }
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        EntityManager em = getEntityManager();
+        em.joinTransaction();
+        em.merge(author);
+        em.flush();
+        em.clear();
+        return em.find(Author.class, author.getId());
     }
 
     @Override
     public void deleteAuthorById(Long id) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Author author = em.find(Author.class, id);
+        em.remove(author);
+        em.flush();
+        em.getTransaction().commit();
+    }
 
+    private EntityManager getEntityManager(){
+        return emf.createEntityManager();
     }
 }
---
+++ src/test/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/h2/AuthorDaoImplTest.java
@@ -0,0 +1,78 @@
+package ch.dboeckli.guru.jpa.hibernate.dao.dao.h2;
+
+import ch.dboeckli.guru.jpa.hibernate.dao.dao.AuthorDao;
+import ch.dboeckli.guru.jpa.hibernate.dao.dao.AuthorDaoImpl;
+import ch.dboeckli.guru.jpa.hibernate.dao.domain.Author;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@Import({ AuthorDaoImpl.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+@Slf4j
+class AuthorDaoImplTest {
+
+    @Autowired
+    AuthorDao authorDao;
+
+    @Test
+    void testDeleteAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        authorDao.deleteAuthorById(saved.getId());
+
+        Author deleted = authorDao.getById(saved.getId());
+        assertThat(deleted).isNull();
+        assertNull(authorDao.getById(saved.getId()));
+    }
+
+    @Test
+    void testUpdateAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        saved.setLastName("Thompson");
+        Author updated = authorDao.updateAuthor(saved);
+
+        assertThat(updated.getLastName()).isEqualTo("Thompson");
+    }
+
+    @Test
+    void testSaveAuthor() {
+        Author author = new Author();
+        author.setFirstName("John");
+        author.setLastName("Thompson");
+        Author saved = authorDao.saveNewAuthor(author);
+
+        assertThat(saved).isNotNull();
+        assertNotNull(saved.getId());
+    }
+
+    @Test
+    void testGetAuthorByName() {
+        Author author = authorDao.findAuthorByName("Craig", "Walls");
+        assertThat(author).isNotNull();
+    }
+
+    @Test
+    void testGetAuthor() {
+        Author author = authorDao.getById(1L);
+        assertThat(author).isNotNull();
+    }
+
+}
